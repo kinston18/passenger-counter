8966ea3e7739dcdf32c3d7df4439a2ab2ca36eb5
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-03-23 00:41:54 +0530

    Final look

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
+console.log(saveEl)
+
 function increment() {
-    count = count + 1
+    count += 1
     countEl.innerText = count
 }
 
-// 1. Create a function, save(), which logs out the count when it's called
-
-
-function save(){
+function save() {
+    let countStr = count + " - "
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+    saveEl.innerText += countStr
+    // NB: Make sure to not delete the existing content of the paragraph
     console.log(count)
 }
+
 
