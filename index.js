6285fdae8cb39a3e65b8dfeee0e406bcefd14cf1
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-03-22 22:19:14 +0530

    Function making

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 //document.getElementById("count-el").innerText = 6
 
 
-let count = 0
+//let count = 5
+//count=count+1
+//console.log(count)
 
-console.log(count)
+
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+function increment()
+{
+    console.log("The button was clicked")
+}
