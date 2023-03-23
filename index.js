76e2bc68f4174ee36fa1ff9fd317ef4dd1537264
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-03-23 19:25:30 +0530

    made the counter

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function save() {
     let countStr = count + " - "
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
     // 3. Render the variable in the saveEl using innerText
-    saveEl.innerText += countStr
+    saveEl.innerText += countStr 
     // NB: Make sure to not delete the existing content of the paragraph
     console.log(count)
 }
